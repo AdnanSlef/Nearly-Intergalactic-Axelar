45eaffb4f6ed92b666e71ab7899603b3e613082a
Vijay <sccr247@gmail.com>
2023-05-21 02:55:48 -0400

    treasure map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import Home from "./components/home";
+import SvgComponent from "./components/TreasureMap";
 
 function App() {
   return (
     <>
+      <SvgComponent />
       <Home />
     </>
   );
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ProgressBar from "@ramonak/react-progress-bar";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <ProgressBar completed={40} />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
