c254cefa0b092ab334970602c47a91c03393c162
Vijay <sccr247@gmail.com>
2023-05-21 05:45:19 -0400

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import "./background-space.css";
 function App() {
   return (
     <div className="space-background">
+      <h1 className="title">AXELAR NEAR GALACTIC PORTAL</h1>
       <Home />
     </div>
   );
